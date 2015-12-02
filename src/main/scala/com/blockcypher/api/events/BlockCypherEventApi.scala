e29ed65727db7c46a62b779e6daca4ba3660786e
Chris Stewart <stewart.chris1234@gmail.com>
2015-12-02 10:26:00 -0600

    Adding scalacoin dependency

---
+++ src/main/scala/com/blockcypher/api/events/BlockCypherEventApi.scala
@@ -0,0 +1,10 @@
+package com.blockcypher.api.events
+
+import com.blockcypher.api.config.BlockCypherEnvironment
+
+/**
+ * Created by chris on 12/2/15.
+ */
+trait BlockCypherEventApi { this : BlockCypherEnvironment =>
+
+}
