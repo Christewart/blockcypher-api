e99bc74a75650493659e8cee82283106993ae936
Chris Stewart <stewart.chris1234@gmail.com>
2015-12-02 09:57:57 -0600

    Adding blockcypher environment

---
+++ src/main/scala/com/blockcypher/api/config/BlockCypherEnvironment.scala
@@ -0,0 +1,29 @@
+package com.blockcypher.api.config
+
+/**
+ * Created by chris on 12/2/15.
+ */
+trait BlockCypherEnvironment {
+
+  private def blockCypherUrl = "https://api.blockcypher.com/"
+  def version = "v1/"
+  def coin : String
+  def network : String
+
+  def baseUrl = blockCypherUrl + version + coin + network
+
+  def webHooks(token : String) = baseUrl + "/hooks?token=" + token
+
+
+}
+
+
+trait BlockCypherMainNet extends BlockCypherEnvironment {
+  override def coin = "btc/"
+  override def network = "main"
+}
+
+trait BlockCypherTestNet extends BlockCypherEnvironment {
+  override def coin = "btc/"
+  override def network = "test3"
+}
---
+++ src/test/scala/com/blockcypher/api/config/BlockCypherEnvironmentTest.scala
@@ -0,0 +1,22 @@
+package com.blockcypher.api.config
+
+import org.scalatest.{FlatSpec, MustMatchers}
+
+/**
+ * Created by chris on 12/2/15.
+ */
+class BlockCypherEnvironmentTest extends FlatSpec with MustMatchers with BlockCypherMainNet {
+
+
+  "BlockCypherEnvironment" must "build a correct base url" in {
+    val expectedUrl = "https://api.blockcypher.com/v1/btc/main"
+    baseUrl must be (expectedUrl)
+  }
+
+  it must "build a correct web hook url" in {
+    val expectedUrl = "https://api.blockcypher.com/v1/btc/main/hooks?token=YOURTOKEN"
+
+    webHooks("YOURTOKEN") must be (expectedUrl)
+  }
+
+}
